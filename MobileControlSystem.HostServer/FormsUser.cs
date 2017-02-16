1150ae2acab070d378f1aedd0f4d46eb1c6130fe
ZhangKong <602488225@qq.com>
2017-02-16 17:05:49 +0800

    Nancy  SelfHost

---
+++ MobileControlSystem.HostServer/FormsUser.cs
@@ -0,0 +1,16 @@
+﻿using Nancy.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileControlSystem.HostServer
+{
+    public class FormsUser : IUserIdentity
+    {
+        public string UserName { get; set; }
+
+        public IEnumerable<string> Claims { get; set; }
+    }
+}
